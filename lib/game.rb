880e86d047512ef79f46259d4a5bccd1f294135f
Andre Menezes <djdedemenezes@gmail.com>
2022-01-29 05:29:03 -0300

    can move snake head around

--- lib/game.rb
+++ lib/game.rb
@@ -10,10 +10,21 @@ class GameWindow < Gosu::Window
   end
 
   def update
+    @snake.update
   end
 
   def draw
     @snake.draw
+  end
+
+  def button_down(id)
+    case id
+    when Gosu::KB_UP then @snake.move_up
+    when Gosu::KB_LEFT then @snake.move_left
+    when Gosu::KB_DOWN then @snake.move_down
+    when Gosu::KB_RIGHT then @snake.move_right
+    else
+    end
   end
 end
 
--- lib/snake.rb
+++ lib/snake.rb
@@ -1,16 +1,56 @@
+require 'pry-byebug'
+
 class Snake
   SIZE = 20
+
   def initialize
     @x = 0
     @y = 0
     @vel_x = 0
     @vel_y = 0
+    @tail = []
 
+    @speed = 0.1
+    p @speed
   end
 
-  def update;end
+  def update
+    @x += @vel_x
+    @y += @vel_y
+  end
 
   def draw
     Gosu.draw_rect(@x, @y, SIZE, SIZE, Gosu::Color::GREEN)
+  end
+
+  def move(direction = {})
+    p direction
+    set_direction
+    update
+  end
+
+  def move_up
+    @vel_x = 0
+    @vel_y = -1
+  end
+
+  def move_left
+    @vel_x = -1
+    @vel_y = 0
+  end
+
+  def move_down
+    @vel_x = 0
+    @vel_y = 1
+  end
+
+  def move_right
+    @vel_x = 1
+    @vel_y = 0
+  end
+
+  def set_direction
+    moving_axis = [@vel_y, @vel_x].select { |vel| vel != 0 }.first
+    p moving_axis
   end
 end
